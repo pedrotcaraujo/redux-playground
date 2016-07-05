688d807d839fe947a73eee66501de48cfacc8cd7
Pedro Araujo <pedrotcaraujo@gmail.com>
2016-07-05 10:12:29 -0300

    Adding Form component

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,40 +1,16 @@
 import React from 'react';
-import {store} from '../store';
+import Form from './Form.jsx';
 
 class Auth extends React.Component {
     constructor() {
         super()
     }
 
-    handleLogin() {
-        let username = this.refs.username.value;
-        store.dispatch({
-            type: 'LOGIN',
-            value: username
-        });
-    }
-
-    handleLogout() {
-        store.dispatch({
-            type: 'LOGOUT',
-            value: 'guest'
-        });
-        this.refs.username.value = '';
-    }
-
-    render() {
-        let button;
-        if(this.props.state.status === 'logged in') {
-            button = <button onClick={this.handleLogout.bind(this)}>Log out</button>;  
-        } else {
-            button = <input type="button" value="Login" onClick={this.handleLogin.bind(this)} />
-        };
-    
+    render() {    
         return (
             <div>
-                <input type="text" ref="username" />
-                {button} 
-                <h1>Current state is {this.props.state.status + ' as ' + this.props.state.value}</h1>
+                <Form state={this.props.state} />
+                
             </div>
         );
     }
